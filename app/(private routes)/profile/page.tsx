3840c8741f7d96b73f5e1b826b98f256a59a6840
Andrii <semtrololo@gmail.com>
2025-07-21 15:33:42 +0200

    add metadata in Profile

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -2,6 +2,25 @@ import Image from "next/image";
 import { getMe } from "@/lib/api/serverApi";
 import css from "./ProfilePage.module.css";
 import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Your Profile — NoteHub",
+  description: "View and manage your profile information.",
+  openGraph: {
+    title: "Your Profile — NoteHub",
+    description: "View and manage your profile information.",
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "/op_profile.png",
+        width: 1200,
+        height: 630,
+        alt: "User Profile Page on NoteHub",
+      },
+    ],
+  },
+};
 
 export default async function Profile() {
   const user = await getMe();
